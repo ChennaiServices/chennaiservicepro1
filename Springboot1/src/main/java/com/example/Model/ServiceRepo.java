13b298fcde543f0e96f565da439331c571ea0001
GO-MADsolutions <gomadsolutions@gmail.com>
2016-09-19 18:54:28 +0530

    Demo Commit 1

--- Springboot1/src/main/java/com/example/Model/ServiceRepo.java
+++ Springboot1/src/main/java/com/example/Model/ServiceRepo.java
@@ -2,7 +2,7 @@ package com.example.Model;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
